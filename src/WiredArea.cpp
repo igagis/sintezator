6feffdafcd2da2b3c8f67891b0785490bd72c64b
Ivan Gagis <igagis@gmail.com>
2017-07-05 11:38:28 +0300

    stuff

--- src/WiredArea.cpp
+++ src/WiredArea.cpp
@@ -1,5 +1,6 @@
 #include "WiredArea.hpp"
 
+#include <morda/util/util.hpp>
 
 WiredArea::WiredArea(const stob::Node* chain) :
 		Widget(chain)
@@ -11,5 +12,17 @@ WiredArea::WiredArea(const stob::Node* chain) :
 WiredArea::WireSocket::WireSocket(const stob::Node* chain) :
 		Widget(chain)
 {
-	
+	if (auto p = morda::getProperty(chain, "wireOut")) {
+		if (std::string("left") == p->value()) {
+			this->wireOut_v = WireOut_e::LEFT;
+		}else if (std::string("right") == p->value()) {
+			this->wireOut_v = WireOut_e::RIGHT;
+		}else if (std::string("top") == p->value()) {
+			this->wireOut_v = WireOut_e::TOP;
+		}else if (std::string("bottom") == p->value()) {
+			this->wireOut_v = WireOut_e::BOTTOM;
+		}else{
+			this->wireOut_v = WireOut_e::CENTER;
+		}
+	}
 }
--- src/WiredArea.hpp
+++ src/WiredArea.hpp
@@ -13,11 +13,31 @@ public:
 	
 	class WireSocket : virtual public morda::Widget{
 	public:
+		enum class WireOut_e {
+			LEFT,
+			RIGHT,
+			TOP,
+			BOTTOM,
+			CENTER
+		};
+	private:
+		WireOut_e wireOut_v;
+	public:
 		WireSocket(const WireSocket&) = delete;
 		WireSocket& operator=(const WireSocket&) = delete;
 		
 		WireSocket(const stob::Node* chain = nullptr);
 
+		/**
+		 * @brief Alignment of wire out.
+		 */
+		decltype(wireOut_v) wireOut()const noexcept {
+			return this->wireOut_v;
+		}
+
+		void setWireOut(decltype(wireOut_v) wo) {
+			this->wireOut_v = wo;
+		}
 	};
 	
 private:
