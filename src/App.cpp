76a599ea848448e8578a5de369fc20c4e02629bb
Ivan Gagis <igagis@gmail.com>
2018-01-23 01:06:07 +0200

    stuff

--- src/App.cpp
+++ src/App.cpp
@@ -11,17 +11,17 @@ App::App() :
 
 	//		morda::inst().resMan.mountResPack(*this->getResFile("res/"));
 
-	morda::inst().inflater.addWidget<WiredArea>("WiredArea");
-	morda::inst().inflater.addWidget<WiredArea::WireSocket>("WireSocket");
+	morda::inst().inflater.registerType<WiredArea>("WiredArea");
+	morda::inst().inflater.registerType<WiredArea::WireSocket>("WireSocket");
 
 	auto c = morda::Morda::inst().inflater.inflate(
 			*this->getResFile("res/main.gui")
 		);
 	
 	{
-		auto in11 = c->findByName("in11")->findChildByNameAs<WiredArea::WireSocket>("ws");
+		auto in11 = c->findByName("in11")->findByNameAs<WiredArea::WireSocket>("ws");
 		ASSERT(in11)
-		auto in12 = c->findByName("in12")->findChildByNameAs<WiredArea::WireSocket>("ws");
+		auto in12 = c->findByName("in12")->findByNameAs<WiredArea::WireSocket>("ws");
 		ASSERT(in12)
 		in11->connect(in12);
 	}
